32e4ead9f76ecda6c0d34c5ae44493f21865de21
Edward Tunnah <edge226@users.noreply.github.com>
2014-04-09 02:30:43 -0600

    fix mounts.

--- manjaro-bootstrap.sh
+++ manjaro-bootstrap.sh
@@ -8,7 +8,7 @@
 FIRST_PACKAGE=(filesystem)
 BASH_PACKAGES=(glibc ncurses readline bash)
 PACMAN_PACKAGES=(acl archlinux-keyring manjaro-keyring attr bzip2 coreutils curl e2fsprogs expat gnupg gpgme keyutils krb5 libarchive libassuan libgpg-error libgcrypt libssh2 lzo2 openssl pacman xz zlib)
-CORE_PACKAGES=(pacman-mirrorlist tar libcap arch-install-scripts util-linux systemd)
+CORE_PACKAGES=(pacman-mirrorlist tar libcap arch-install-scripts util-linux systemd nano vim)
 # EXTRA_PACKAGES=(manjaroiso git base base-devel)
 PACKAGES=(${FIRST_PACKAGE[*]} ${BASH_PACKAGES[*]} ${PACMAN_PACKAGES[*]} ${CORE_PACKAGES[*]})
 
@@ -42,11 +42,11 @@ done
 #        rm -f "$CHROOT_DIR/.PKGINFO" "$CHROOT_DIR/.MTREE" "$CHROOT_DIR/.INSTALL"
 #done
 # Create mount points
-mount -t proc proc "$CHROOT_DIR/proc/"
-mount -rbind /sys "$CHROOT_DIR/sys/"
-mount -rbind /dev "$CHROOT_DIR/dev/"
+mount -o bind /proc "$CHROOT_DIR/proc/"
+mount -o bind /sys "$CHROOT_DIR/sys/"
+mount -o bind /dev "$CHROOT_DIR/dev/"
 mkdir -p "$CHROOT_DIR/dev/pts"
-mount -t devpts pts "$CHROOT_DIR/dev/pts/"
+mount -t devpts devpts "$CHROOT_DIR/dev/pts/"
 
 # Hash for empty password  Created by doing: openssl passwd -1 -salt ihlrowCo and entering an empty password (just press enter)
 # echo 'root:$1$ihlrowCo$sF0HjA9E8up9DYs258uDQ0:10063:0:99999:7:::' > "$CHROOT_DIR/etc/shadow"
